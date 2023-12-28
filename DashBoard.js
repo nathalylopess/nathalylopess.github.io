21c17d8ba4595352255c351fd04aed5927e3529b
nathaly lopess <129780610+nathalylopess@users.noreply.github.com>
2023-12-28 15:44:19 -0300

    Update DashBoard.js

--- DashBoard.js
+++ DashBoard.js
@@ -1,21 +1,21 @@
 var carrinho = document.getElementById('carrinho_pag');
 carrinho.addEventListener("click", function(){
-    window.location.href =
+    window.location.href = ""
 })
 
 var carrinho_responsivo = document.getElementById('carrinho_pag_responsivo');
 carrinho_responsivo.addEventListener("click", function(){
-    window.location.href =
+    window.location.href = ""
 })
 
 var login = document.getElementById('login_pag');
 login.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = ""
 })
 
 var login_responsivo = document.getElementById('login_pag_responsivo');
 login_responsivo.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = ""
 })
 
 var whey = document.getElementById('whey');
@@ -25,16 +25,16 @@ whey.addEventListener("click", function(){
 
 var creatina = document.getElementById('creatina');
 creatina.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = ""
 })
 
 var acessorios = document.getElementById('acessorios');
 acessorios.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = ""
 })
 
 var dashboard = document.getElementById('dashboard');
 dashboard.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = ""
 })
 
