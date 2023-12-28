ccb82913dad9dcf848992a9119476b5b20ae0d10
nathaly lopess <129780610+nathalylopess@users.noreply.github.com>
2023-12-28 15:57:37 -0300

    Update pagina_de_entrada.js

--- pagina_de_entrada.js
+++ pagina_de_entrada.js
@@ -20,45 +20,41 @@ carrosel.addEventListener('mouseleave', () => intervalo = setInterval(proximosli
 
 var carrinho = document.getElementById('carrinho_pag');
 carrinho.addEventListener("click", function(){
-    window.location.href =
+    window.location.href = "https://nathalylopess.github.io/carrinho.html"
 })
 
 var carrinho_responsivo = document.getElementById('carrinho_pag_responsivo');
 carrinho_responsivo.addEventListener("click", function(){
-    window.location.href =
+    window.location.href = "https://nathalylopess.github.io/carrinho.html"
 })
 
 var login = document.getElementById('login_pag');
 login.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = "https://nathalylopess.github.io/pagina_de_login.html"
 })
 
 var login_responsivo = document.getElementById('login_pag_responsivo');
 login_responsivo.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = "https://nathalylopess.github.io/pagina_de_login.html"
 })
 
-//var whey = document.getElementById("whey");
-//whey.addEventListener("click", function(){
-  //window.location.href = "https://nathalylopess.github.io/whey_protein.html"
-//})
-
-var texto = "whey";
-var url = "https://nathalylopess.github.io/whey_protein.html";
-document.write(texto.link(url));
+var whey = document.getElementById("whey");
+whey.addEventListener("click", function(){
+  window.location.href = "https://nathalylopess.github.io/whey_protein.html"
+})
 
 var creatina = document.getElementById('creatina');
 creatina.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = "https://nathalylopess.github.io/creatinas.html"
 })
 
 var acessorios = document.getElementById('acessorios');
 acessorios.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = "https://nathalylopess.github.io/acessorios.html"
 })
 
 var dashboard = document.getElementById('dashboard');
 dashboard.addEventListener("click", function(){
-  window.location.href =
+  window.location.href = "https://nathalylopess.github.io/DashBoard.html"
 })
 
