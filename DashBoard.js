913686b1fb4f3287a334b7509248138d0c7a433a
Riversson Paulo <150279469+riverssonpaulo@users.noreply.github.com>
2023-12-28 15:39:16 -0300

    Add files via upload

---
+++ DashBoard.js
@@ -0,0 +1,40 @@
+var carrinho = document.getElementById('carrinho_pag');
+carrinho.addEventListener("click", function(){
+    window.location.href =
+})
+
+var carrinho_responsivo = document.getElementById('carrinho_pag_responsivo');
+carrinho_responsivo.addEventListener("click", function(){
+    window.location.href =
+})
+
+var login = document.getElementById('login_pag');
+login.addEventListener("click", function(){
+  window.location.href =
+})
+
+var login_responsivo = document.getElementById('login_pag_responsivo');
+login_responsivo.addEventListener("click", function(){
+  window.location.href =
+})
+
+var whey = document.getElementById('whey');
+whey.addEventListener("click", function(){
+  window.location.href =
+})
+
+var creatina = document.getElementById('creatina');
+creatina.addEventListener("click", function(){
+  window.location.href =
+})
+
+var acessorios = document.getElementById('acessorios');
+acessorios.addEventListener("click", function(){
+  window.location.href =
+})
+
+var dashboard = document.getElementById('dashboard');
+dashboard.addEventListener("click", function(){
+  window.location.href =
+})
+
