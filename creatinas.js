d5064f0d5dce85a7704517f9e7e8c2ed8291913e
Riversson Paulo <150279469+riverssonpaulo@users.noreply.github.com>
2023-12-29 13:33:53 -0300

    Update creatinas.js

--- creatinas.js
+++ creatinas.js
@@ -1,5 +1,14 @@
 var carrinho = document.getElementById('carrinho_pag');
-carrinho.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+carrinho.addEventListener("click", function(){
+    window.location.href = "https://nathalylopess.github.io/carrinho.html"
+})
 
 var login = document.getElementById('login_pag');
-login.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"><i class="bx bx-user bx-md "></i></a>'
+login.addEventListener("click", function(){
+  window.location.href = "https://nathalylopess.github.io/pagina_de_login.html"
+})
+
+var inicial = document.getElementById('inicial');
+inicial.addEventListener("click", function(){
+  window.location.href = "https://nathalylopess.github.io/index.html"
+})
